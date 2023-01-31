71813e89c2fff49d3432641ea69d1569a0d5f797
sujungH <hsj415415@naver.com>
2023-01-31 14:56:19 +0900

    total_project

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def print_hi(name):
 #수정
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+#지민
+def a(a,b):
+    c=a+b
+    return (c)
+
+def b(a, b):
+    return (a+b, a-b)
+
+#윤호
+print(a(1,2))
+print(b(5,4))
